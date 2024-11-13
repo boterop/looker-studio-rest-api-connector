9f79cbab604248c4c91ac4756ced80fcd42dbc4c
boterop <boterop22@gmail.com>
2024-11-12 20:01:29 -0500

    Login in sgsst api

--- src/auth.js
+++ src/auth.js
@@ -1,64 +1,7 @@
 const getAuthType = () =>
   communityConnector
     .newAuthTypeResponse()
-    .setAuthType(communityConnector.AuthType.USER_PASS)
-    .setHelpUrl(`${BASE_URL}/api-docs`)
+    .setAuthType(communityConnector.AuthType.NONE)
     .build();
 
-const isAuthValid = () => {
-  const userProperties = PropertiesService.getUserProperties();
-  const companyId = userProperties.getProperty("dscc.company_id");
-  const userName = userProperties.getProperty("dscc.username");
-  const password = userProperties.getProperty("dscc.password");
-
-  return login(companyId, userName, password);
-};
-
-const setCredentials = (request) => {
-  const { username, password } = request.userPass;
-
-  const userProperties = PropertiesService.getUserProperties();
-  userProperties.setProperty("dscc.username", username);
-  userProperties.setProperty("dscc.password", password);
-  return { errorCode: "NONE" };
-};
-
-const resetAuth = () => {
-  const userProperties = PropertiesService.getUserProperties();
-  // userProperties.deleteProperty("dscc.company_id");
-  userProperties.deleteProperty("dscc.username");
-  userProperties.deleteProperty("dscc.password");
-};
-
-// Check if the current user has administrative privileges
-const isAdminUser = () => {
-  // For this example, all users are treated as admin users
-  return true;
-};
-
-// User-defined functions
-
-const login = (companyId, userName, password) => {
-  const url = `${BASE_URL}/seguridad/login`;
-
-  if (!companyId || userName || password) {
-    return false;
-  }
-
-  const payload = {
-    id_empresa: companyId,
-    usuario: userName,
-    clave: password,
-  };
-
-  console.log(payload);
-  // Fetch and parse the API response
-  const response = UrlFetchApp.fetch(url, {
-    method: "POST",
-    payload: JSON.stringify(payload),
-    contentType: "application/json",
-  });
-  const parsedResponse = JSON.parse(response);
-
-  return parsedResponse.success;
-};
+const isAdminUser = () => true;
--- src/code.js
+++ src/code.js
@@ -25,11 +25,6 @@ const schema = [
   },
 ];
 
-// Return the defined schema to Data Studio
-const getSchema = (request) => {
-  return { schema: schema };
-};
-
 // Define the configuration settings for the connector, including user input fields
 const getConfig = (request) => {
   const config = communityConnector.getConfig();
@@ -42,17 +37,52 @@ const getConfig = (request) => {
     );
 
   // Basic authentication
-  createInput(config, "Company ID", "Enter your Company ID", false);
-  createInput(config, "Username", "Enter your API username", false);
-  createInput(config, "Password", "Enter your API password", false);
+  createInput(config, "Company ID", "Enter your Company ID", {
+    override: false,
+    required: true,
+  });
+  createInput(config, "Username", "Enter your API username", {
+    override: false,
+    required: true,
+  });
+  createInput(config, "Password", "Enter your API password", {
+    override: false,
+    required: true,
+  });
 
+  // Parameters
   createInput(
     config,
-    "API URL",
-    "Enter the API URL for fetching siag data. Default is /pesv/vehiculo"
+    "Endpoint",
+    "Enter the endpoint for fetching siag data. Default is /pesv/vehiculo",
+    {
+      required: true,
+    }
   );
 
   return config.build();
+};
+
+// Return the defined schema to Data Studio
+const getSchema = (request) => {
+  const requiredError = { errorCode: "Please enter all required fields" };
+  if (!request.configParams) {
+    return requiredError;
+  }
+
+  const { endpoint, company_id, user_name, password } = request.configParams;
+
+  if (!endpoint || !company_id || !user_name || !password) {
+    return requiredError;
+  }
+
+  const token = login(company_id, user_name, password);
+
+  if (!token) {
+    return { errorCode: "INVALID_CREDENTIALS" };
+  }
+
+  return { schema: schema };
 };
 
 const getData = (request) => {
@@ -64,11 +94,10 @@ const getData = (request) => {
     semantics: { conceptType: "DIMENSION" },
   }));
 
-  // Construct the API URL based on user input
-  const url = `${BASE_URL}${request.configParams.apiUrl}`;
-
   // Fetch and parse the API response
-  const response = UrlFetchApp.fetch(url);
+  const response = UrlFetchApp.fetch(
+    `${BASE_URL}${request.configParams.apiUrl}`
+  );
   const parsedResponse = JSON.parse(response);
 
   // Map the API response to the schema for Data Studio
@@ -82,75 +111,42 @@ const getData = (request) => {
   };
 };
 
-// Specify the authentication type for the connector
-const getAuthType = () =>
-  communityConnector
-    .newAuthTypeResponse()
-    .setAuthType(communityConnector.AuthType.USER_PASS)
-    .setHelpUrl(`${BASE_URL}/api-docs`)
-    .build();
-
-const isAuthValid = () => {
-  const userProperties = PropertiesService.getUserProperties();
-  const companyId = userProperties.getProperty("dscc.company_id");
-  const userName = userProperties.getProperty("dscc.username");
-  const password = userProperties.getProperty("dscc.password");
-
-  return login(companyId, userName, password);
-};
-
-const setCredentials = (request) => {
-  const { username, password } = request.userPass;
-
-  const userProperties = PropertiesService.getUserProperties();
-  userProperties.setProperty("dscc.username", username);
-  userProperties.setProperty("dscc.password", password);
-  return { errorCode: "NONE" };
-};
-
-const resetAuth = () => {
-  const userProperties = PropertiesService.getUserProperties();
-  // userProperties.deleteProperty("dscc.company_id");
-  userProperties.deleteProperty("dscc.username");
-  userProperties.deleteProperty("dscc.password");
-};
-
-// Check if the current user has administrative privileges
-const isAdminUser = () => {
-  // For this example, all users are treated as admin users
-  return true;
-};
-
 // User-defined functions
 
 const login = (companyId, userName, password) => {
   const url = `${BASE_URL}/seguridad/login`;
+
+  if (!companyId || !userName || !password) {
+    return null;
+  }
+
   const payload = {
     id_empresa: companyId,
     usuario: userName,
     clave: password,
   };
 
-  Logger.log(url);
-  Logger.log(payload);
+  try {
+    const response = UrlFetchApp.fetch(url, {
+      method: "POST",
+      payload: JSON.stringify(payload),
+      contentType: "application/json",
+    });
 
-  // Fetch and parse the API response
-  const response = UrlFetchApp.fetch(url, {
-    method: "POST",
-    payload: JSON.stringify(payload),
-    contentType: "application/json",
-  });
-  const parsedResponse = JSON.parse(response);
+    const { data } = JSON.parse(response);
 
-  return parsedResponse.success;
+    return data;
+  } catch (error) {
+    return null;
+  }
 };
 
 const idlize = (str) => str.toLowerCase().replace(/[^a-z0-9]/g, "_");
 
-const createInput = (config, name, helpText, override = true) =>
+const createInput = (config, name, helpText, args = {}) =>
   config
     .newTextInput()
     .setId(idlize(name))
-    .setName(name)
+    .setName(`${name} ${args.required ? "*" : ""}`)
     .setHelpText(helpText)
-    .setAllowOverride(override);
+    .setAllowOverride(args.override || true);
